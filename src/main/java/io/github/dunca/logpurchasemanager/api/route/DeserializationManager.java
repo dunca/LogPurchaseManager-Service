d8207dffd1e20e9607242e3899d6fe1f3351bf9d
dunca <flv@mail.com>
2018-04-20 13:51:57 +0300

    DeserializationManager: initial implementation

---
+++ src/main/java/io/github/dunca/logpurchasemanager/api/route/DeserializationManager.java
@@ -0,0 +1,46 @@
+package io.github.dunca.logpurchasemanager.api.route;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.dunca.logpurchasemanager.api.route.response.exceptions.InvalidModelException;
+
+import java.io.IOException;
+import java.util.List;
+
+public class DeserializationManager {
+    private final ObjectMapper objectMapper;
+    private static DeserializationManager instance;
+
+    private DeserializationManager() {
+        objectMapper = new ObjectMapper();
+    }
+
+    public static DeserializationManager getInstance() {
+        if (instance == null) {
+            instance = new DeserializationManager();
+        }
+
+        return instance;
+    }
+
+    public <T, R> R deserialize(String json, Class<T> modelClass, boolean isList) {
+        if (json == null) {
+            throw new InvalidModelException();
+        }
+
+        JavaType javaType;
+
+        if (isList) {
+            javaType = objectMapper.getTypeFactory().constructCollectionType(List.class, modelClass);
+        } else {
+            javaType = objectMapper.getTypeFactory().constructType(modelClass);
+        }
+
+        try {
+            return objectMapper.readValue(json, javaType);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new InvalidModelException();
+        }
+    }
+}
