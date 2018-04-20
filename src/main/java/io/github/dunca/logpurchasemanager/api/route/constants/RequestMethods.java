31142d9b0ac49b9dd4506738b0b13344fa7649dc
dunca <flv@mail.com>
2018-04-20 10:24:20 +0300

    RequestMethods: initial implementation

---
+++ src/main/java/io/github/dunca/logpurchasemanager/api/route/constants/RequestMethods.java
@@ -0,0 +1,11 @@
+package io.github.dunca.logpurchasemanager.api.route.constants;
+
+public class RequestMethods {
+    private RequestMethods() {
+
+    }
+
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    public static final String PATCH = "PATCH";
+}
