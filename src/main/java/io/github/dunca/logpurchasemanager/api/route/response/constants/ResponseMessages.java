b1991522571b5669e35e478aab67908f415c46fe
dunca <flv@mail.com>
2018-04-20 10:26:23 +0300

    ResponseMessages: initial implementation

---
+++ src/main/java/io/github/dunca/logpurchasemanager/api/route/response/constants/ResponseMessages.java
@@ -0,0 +1,10 @@
+package io.github.dunca.logpurchasemanager.api.route.response.constants;
+
+public class ResponseMessages {
+    private ResponseMessages() {
+
+    }
+
+    public static final String SERIALIZATION_ERROR = "{\"error\":\"error while trying to serialize data\"}";
+}
+
